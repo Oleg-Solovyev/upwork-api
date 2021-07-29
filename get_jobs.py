ef7a8b16b1d49fd47281e2fa35b9a3a57e9cf666
Oleg Solovyev <oleg.s.solovyev@gmail.com>
2021-07-29 13:30:29 +0300

    added loop over tech list

--- get_jobs.py
+++ get_jobs.py
@@ -1,3 +1,6 @@
+# https://developers.upwork.com
+
+
 import upwork
 from pprint import pprint
 from upwork.routers import auth
@@ -7,7 +10,12 @@ from upwork.routers.activities import team
 from upwork.routers.reports import time
 from urllib.parse import quote
 import pandas as pd
+from datetime import date
 import csv
+
+
+# constants
+TECH_LIST = ['Talend', 'Informatica', 'Datastage', 'Redshift', 'ODI', 'ETL', 'DWH', 'data warehouse']
 
 
 def get_desktop_client():
@@ -15,11 +23,7 @@ def get_desktop_client():
     Your key should be created with the project type "Desktop".
     Returns: ``upwork.Client`` instance ready to work.
     """
-    print("Emulating desktop app")
-    
-    #consumer_key = input('Please enter consumer key: > ')
-    #consumer_secret = input('Please enter key secret: > ')
-    #config = upwork.Config({'consumer_key': consumer_key, 'consumer_secret': consumer_secret})
+
     config = upwork.Config({'consumer_key': '0be06c2b526776ff48a458afac4b7c22', 'consumer_secret': '96d4284884277b83'})
     """Assign access_token and access_token_secret if they are known
     config = upwork.Config({\
@@ -44,54 +48,27 @@ def get_desktop_client():
         access_token, access_token_secret = client.get_access_token(verifier)
         print('OK')
     
-    # For further use you can store ``access_toket`` and
-    # ``access_token_secret`` somewhere
-    
     return client
+
 
 if __name__ == '__main__':
     client = get_desktop_client()
     
     try:
-        print("My info")
-        pprint(auth.Api(client).get_user_info())
-        # docs: https://developers.upwork.com/?lang=python#jobs_search-for-jobs
-        resp = search.Api(client).find({'q': 'Talend', 'paging':'0;100'})
-        #pprint(search.Api(client).find({'q': 'opencart'}))
-        
-        #jobs = search.Api(client).find({'q': 'kotlin'})
-        #pprint(jobs)
-        #with open('jobs.txt', 'w') as outfile:
-        #    json.dump(jobs, outfile)
-        
-        # This APP has no access to requested resource    
-        #data = team.Api(client).add_activity('mycompany', 'mycompany', {'code': 'team-task-001', 'description': 'Description', 'all_in_company': '1'})
-        #pprint(data)
-        #with open('data.txt', 'w') as outfile:
-        #    json.dump(data, outfile)
-        
-        #pprint(time.Gds(client).get_by_freelancer_full('oleg-solovyev', {'tq': quote('SELECT task, memo WHERE worked_on >= "2020-05-01" AND worked_on <= "2020-06-01"')}))
+        with open(date.today().strftime('%Y-%m-%d') + '.csv', 'w') as f:
+            for tech in TECH_LIST:
+                resp = search.Api(client).find({'q':tech, 'paging':'0;100', 'days_posted':1})
+                jobs = resp['jobs']
+
+                if tech == TECH_LIST[0]:
+                    w = csv.DictWriter(f, jobs[0].keys())
+                    w.writeheader()
+
+                for i in range(len(jobs)):
+                    if 'agency'   in jobs[i]['snippet'].lower()\
+                    or 'agencies' in jobs[i]['snippet'].lower():
+                        w.writerow(jobs[i])
         
     except Exception as e:
         print("Catch or log exception details")
         raise e
-    
-# https://developers.upwork.com/?lang=python#contracts-and-offers_list-freelancers-offers
-
-jobs = resp['jobs']
-
-with open('mycsvfile.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
-    w = csv.DictWriter(f, jobs[1].keys())
-    w.writeheader()
-    
-    for i in range(len(jobs)):
-        w.writerow(jobs[i])
-
-
-#with open('clientCountry.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
-#    for i in range(len(jobs)):
-#        f.write(jobs[i]['client']['country'])
-#        f.write('\n')
-
-# https://developers.upwork.com/?lang=python#jobs
-
